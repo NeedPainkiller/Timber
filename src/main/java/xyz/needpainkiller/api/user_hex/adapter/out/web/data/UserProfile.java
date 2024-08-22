6b808705c5a23ddf4f4be8eeb9729c143c183f7f
needpainkiller <needpainkiller6512@gmail.com>
2024-08-23 00:46:07 +0900

    Update : 계정 프로파일 패키지 이관

--- src/main/java/xyz/needpainkiller/api/user_hex/adapter/out/web/data/UserProfile.java
+++ src/main/java/xyz/needpainkiller/api/user_hex/adapter/out/web/data/UserProfile.java
@@ -1,4 +1,4 @@
-package xyz.needpainkiller.api.user.dto;
+package xyz.needpainkiller.api.user_hex.adapter.out.web.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
