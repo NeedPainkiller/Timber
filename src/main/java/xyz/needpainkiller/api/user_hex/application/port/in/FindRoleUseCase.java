06f9bcd5a0793c8727ae6a467c56bba3180d008d
needpainkiller <needpainkiller6512@gmail.com>
2024-09-13 00:08:45 +0900

    Update : 권한 캐싱 중복 선언 삭제

--- src/main/java/xyz/needpainkiller/api/user_hex/application/port/in/FindRoleUseCase.java
+++ src/main/java/xyz/needpainkiller/api/user_hex/application/port/in/FindRoleUseCase.java
@@ -1,6 +1,5 @@
 package xyz.needpainkiller.api.user_hex.application.port.in;
 
-import org.springframework.cache.annotation.Cacheable;
 import xyz.needpainkiller.api.user_hex.adapter.in.web.data.RoleRequests;
 import xyz.needpainkiller.api.user_hex.domain.model.Role;
 import xyz.needpainkiller.api.user_hex.domain.model.User;
@@ -14,13 +13,10 @@ public interface FindRoleUseCase {
 
     boolean isRoleExist(Long tenantPk, String roleNm);
 
-    @Cacheable(value = "RoleList", key = "'selectAll'")
     List<Role> selectAll();
 
-    @Cacheable(value = "Role", key = "'selectRoleByRolePk-' + #p0")
     Role selectRoleByRolePk(Long rolePk);
 
-    @Cacheable(value = "Role", key = "'selectRoleByRoleNm-' + #p0")
     Role selectRoleByRoleNm(String roleNm);
 
     SearchCollectionResult<Role> selectRoleList(RoleRequests.SearchRoleRequest param);
@@ -29,28 +25,20 @@ public interface FindRoleUseCase {
 
     List<UserRoleMap> selectUserRoleMap(List<Long> userPkList);
 
-    @Cacheable(value = "UserRole", key = "'selectUserPkListByRolePk-' + #p0")
     List<Long> selectUserPkListByRolePk(Long rolePk);
 
-    @Cacheable(value = "UserRole", key = "'selectUserPkListByRolePkList-' + #p0.hashCode()")
     List<Long> selectUserPkListByRolePkList(List<Long> rolePkList);
 
-    @Cacheable(value = "RoleList", key = "'selectRolesByUser-' + #p0.getId()")
     List<Role> selectRolesByUser(User user);
 
-    @Cacheable(value = "RoleList", key = "'selectRolesByUser-' + #p0")
     List<Role> selectRolesByUser(Long userPk);
 
-    @Cacheable(value = "RoleList", key = "'selectRolesByPkList-' + #p0.hashCode()")
     List<Role> selectRolesByPkList(List<Long> rolePkList);
 
-    @Cacheable(value = "RoleList", key = "'selectRolesByNameList-' + #p0.hashCode()")
     List<Role> selectRolesByNameList(List<String> roleNmList);
 
-    @Cacheable(value = "RoleAuthorityList", key = "'selectAuthorityByUser-' + #p0.getId()")
     List<String> selectAuthorityByUser(User user);
 
-    @Cacheable(value = "RoleAuthorityList", key = "'selectAuthorityByUser-' + #p0")
     List<String> selectAuthorityByUser(Long userPk);
 
     boolean hasSystemAdminRole(List<Role> roleList);
--- src/main/java/xyz/needpainkiller/api/user_hex/application/port/in/ManageRoleUseCase.java
+++ src/main/java/xyz/needpainkiller/api/user_hex/application/port/in/ManageRoleUseCase.java
@@ -11,22 +11,17 @@ import java.util.List;
 
 public interface ManageRoleUseCase {
     @Transactional
-    @Caching(evict = {@CacheEvict(value = "UserRole", allEntries = true), @CacheEvict(value = "Role", allEntries = true), @CacheEvict(value = "RoleList", allEntries = true), @CacheEvict(value = "RoleAuthorityList", allEntries = true)})
     Role createRole(RoleRequests.UpsertRoleRequest param, User requester);
 
     @Transactional
-    @Caching(evict = {@CacheEvict(value = "UserRole", allEntries = true), @CacheEvict(value = "Role", allEntries = true), @CacheEvict(value = "RoleList", allEntries = true), @CacheEvict(value = "RoleAuthorityList", allEntries = true)})
     Role updateRole(Long rolePk, RoleRequests.UpsertRoleRequest param, User requester);
 
     @Transactional
-    @Caching(evict = {@CacheEvict(value = "UserRole", allEntries = true), @CacheEvict(value = "Role", allEntries = true), @CacheEvict(value = "RoleList", allEntries = true), @CacheEvict(value = "RoleAuthorityList", allEntries = true)})
     void deleteRole(Long tenantPk, Long rolePk, User requester);
 
     @Transactional
-    @Caching(evict = {@CacheEvict(value = "UserRole", allEntries = true), @CacheEvict(value = "Role", allEntries = true), @CacheEvict(value = "RoleList", allEntries = true), @CacheEvict(value = "RoleAuthorityList", allEntries = true)})
     void upsertUserRole(Long userPk, List<Role> roleList);
 
     @Transactional
-    @Caching(evict = {@CacheEvict(value = "UserRole", allEntries = true), @CacheEvict(value = "Role", allEntries = true), @CacheEvict(value = "RoleList", allEntries = true), @CacheEvict(value = "RoleAuthorityList", allEntries = true)})
     void deleteUserRole(Long userPk);
 }
