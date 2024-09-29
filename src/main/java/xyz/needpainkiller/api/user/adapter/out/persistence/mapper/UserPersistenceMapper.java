83741246dd1c64626deac6a6389998134f5079b4
needpainkiller <needpainkiller6512@gmail.com>
2024-09-29 20:33:57 +0900

    Add : User 엔티티 매퍼추가

---
+++ src/main/java/xyz/needpainkiller/api/user/adapter/out/persistence/mapper/UserPersistenceMapper.java
@@ -0,0 +1,34 @@
+package xyz.needpainkiller.api.user.adapter.out.persistence.mapper;
+
+import org.mapstruct.Mapper;
+import xyz.needpainkiller.api.tenant.adapter.out.persistence.entity.TenantEntity;
+import xyz.needpainkiller.api.tenant.domain.model.Tenant;
+import xyz.needpainkiller.api.user.adapter.out.persistence.entity.UserEntity;
+import xyz.needpainkiller.api.user.domain.model.User;
+
+
+/**
+ * 테넌트 매퍼
+ * - Persistence -> Domain / Domain -> Persistence
+ *
+ * @author needpainkiller6512
+ * @version 1.0
+ */
+@Mapper(componentModel = "spring")
+public interface UserPersistenceMapper {
+    /**
+     * 테넌트 Entity -> Domain
+     *
+     * @param user 유저
+     * @return TenantEntity 테넌트 Entity
+     */
+    UserEntity toUserEntity(User user);
+
+    /**
+     * 테넌트 Entity -> Domain
+     *
+     * @param userEntity 유저 Entity
+     * @return User 유저
+     */
+    User toTenant(UserEntity userEntity);
+}
