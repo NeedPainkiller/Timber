cc519a70d6118d2abacd5c90f752d014cfbe1fde
needpainkiller <needpainkiller6512@gmail.com>
2024-10-09 03:53:31 +0900

    Update : UserRepository 응답 객체 변경

--- src/main/java/xyz/needpainkiller/api/user/adapter/out/persistence/repository/UserRepository.java
+++ src/main/java/xyz/needpainkiller/api/user/adapter/out/persistence/repository/UserRepository.java
@@ -3,17 +3,18 @@ package xyz.needpainkiller.api.user.adapter.out.persistence.repository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import xyz.needpainkiller.api.user.adapter.out.persistence.entity.UserEntity;
 import xyz.needpainkiller.api.user.domain.model.User;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    List<User> findAll();
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+    List<UserEntity> findAll();
 
-    List<User> findAllByIdIn(List<Long> idList);
+    List<UserEntity> findAllByIdIn(List<Long> idList);
 
 
-    User findUserById(@NotNull Long userPk);
+    UserEntity findUserById(@NotNull Long userPk);
 
-    List<User> findUserByUserId(String userId);
+    List<UserEntity> findUserByUserId(String userId);
 }
