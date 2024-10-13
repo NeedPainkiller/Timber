e9c4701bbddec629b4bd2fc2ab4fbe39d40ea57d
needpainkiller <needpainkiller6512@gmail.com>
2024-10-14 00:41:43 +0900

    Add : UserBeanConfiguration 추가

---
+++ src/main/java/xyz/needpainkiller/api/user/UserBeanConfiguration.java
@@ -0,0 +1,35 @@
+package xyz.needpainkiller.api.user;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import xyz.needpainkiller.api.tenant.adapter.out.event.TenantEventPublisherAdapter;
+import xyz.needpainkiller.api.tenant.adapter.out.persistence.TenantPersistenceAdapter;
+import xyz.needpainkiller.api.tenant.adapter.out.persistence.mapper.TenantPersistenceMapper;
+import xyz.needpainkiller.api.tenant.adapter.out.persistence.repository.TenantRepository;
+import xyz.needpainkiller.api.tenant.domain.service.FindTenantService;
+import xyz.needpainkiller.api.tenant.domain.service.ManageTenantService;
+
+@Configuration
+public class UserBeanConfiguration {
+
+    @Bean
+    public TenantPersistenceAdapter tenantPersistenceAdapter(TenantRepository tenantRepository, TenantPersistenceMapper tenantPersistenceMapper) {
+        return new TenantPersistenceAdapter(tenantRepository, tenantPersistenceMapper);
+    }
+
+    @Bean
+    public TenantEventPublisherAdapter tenantEventPublisherAdapter(ApplicationEventPublisher applicationEventPublisher) {
+        return new TenantEventPublisherAdapter(applicationEventPublisher);
+    }
+
+    @Bean
+    public FindTenantService findTenantService(TenantPersistenceAdapter tenantPersistenceAdapter) {
+        return new FindTenantService(tenantPersistenceAdapter);
+    }
+
+    @Bean
+    public ManageTenantService manageTenantService(TenantPersistenceAdapter tenantPersistenceAdapter, TenantEventPublisherAdapter tenantEventPublisherAdapter) {
+        return new ManageTenantService(tenantPersistenceAdapter, tenantEventPublisherAdapter);
+    }
+}
