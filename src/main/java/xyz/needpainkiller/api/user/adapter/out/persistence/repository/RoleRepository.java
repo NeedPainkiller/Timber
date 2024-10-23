667a4c09cf083b2c76ae7a2e5bd81bc02bbff853
needpainkiller <needpainkiller6512@gmail.com>
2024-10-24 00:24:23 +0900

    Update : RoleRepository 캐싱 제외

--- src/main/java/xyz/needpainkiller/api/user/adapter/out/persistence/repository/RoleRepository.java
+++ src/main/java/xyz/needpainkiller/api/user/adapter/out/persistence/repository/RoleRepository.java
@@ -3,15 +3,16 @@ package xyz.needpainkiller.api.user.adapter.out.persistence.repository;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import xyz.needpainkiller.api.user.adapter.out.persistence.entity.RoleEntity;
 import xyz.needpainkiller.api.user.domain.model.Role;
 
 import java.util.List;
 
-public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+public interface RoleRepository extends JpaRepository<RoleEntity, Long>, JpaSpecificationExecutor<Role> {
 
-    @Cacheable(value = "RoleList", key = "'findAll'")
-    List<Role> findAll();
 
-    @Cacheable(value = "RoleList", key = "'findByIdIn-' + #p0.hashCode()", condition = "#p0!=null")
-    List<Role> findByIdIn(List<Long> idList);
+    List<RoleEntity> findAll();
+
+
+    List<RoleEntity> findByIdIn(List<Long> idList);
 }
